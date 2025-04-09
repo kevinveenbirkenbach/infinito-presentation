4240e8ed035ead2b9a27a544f51c95069464dd41
Kevin Veen-Birkenbach <kevin@veen.world>
2025-04-09 21:48:07 +0200

    Added a structure draft

--- app.py
+++ app.py
@@ -13,35 +13,9 @@ app = Flask(__name__)
 def register_slide_extractor():
     app.jinja_env.globals['extract_slide'] = extract_slide
 
-def find_readmes(source_dir):
-    """Find all README files in a case-insensitive way."""
-    all_files = glob.glob(os.path.join(source_dir, "**/*"), recursive=True)  # Get all files
-    readmes = [f for f in all_files if os.path.basename(f).lower() == "readme.md"]
-
-    # Include other documentation files from the base directory
-    doc_files = glob.glob(f"{source_dir}/*.md")
-    
-    return readmes + doc_files
-
-def get_slide_data(source_dir="/source"):
-    """Generate the presentation HTML from README files using a Jinja2 template file."""
-    readmes = find_readmes(source_dir)
-    slides = []
-
-    for readme in readmes:
-        slides.append(get_slide(readme))
-    return slides
-
-def get_slides_composition():
-    pass
-
 @app.route('/')
 def index():
-    slides = []
-    slides.append(extract_slide("/source/docs/guides/administrator/Readme.md","Key Responsibilities 🔧"))
-    slides.append(extract_slide("/app/templates/start_page.html.j2"))
-    #slides_data = get_slide_data()
-    return render_template('presentation.html.j2', slides=slides)
+    return render_template('presentation.html.j2')
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
