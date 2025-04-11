c41f26fe666c66af203d0d80b5991577f82c1e8c
Kevin Veen-Birkenbach <kevin@veen.world>
2025-04-11 16:09:37 +0200

    Restructured

--- utils/background_helper.py
+++ utils/background_helper.py
@@ -1,7 +1,7 @@
 import os
 from flask import url_for
 
-def get_background(template_folder, file_path):
+def get_background(template_folder:str, file_path:str)->str:
     """
     Returns the background image URL for a given template file.
     Raises an error if the background image does not exist.
@@ -16,10 +16,14 @@ def get_background(template_folder, file_path):
     Raises:
         ValueError: If the corresponding background image is missing.
     """
+    # Strip 'slides/' prefix if present
+    if file_path.startswith("slides/"):
+        file_path = file_path[len("slides/"):]
     background_path = f"img/slides/backgrounds/{file_path}.png"
     static_file_path = os.path.join('static', background_path)
 
     if not os.path.exists(static_file_path):
-        raise ValueError(f"Missing background image: '{static_file_path}' for template '{file_path}'")
+        print(f"Missing background image: '{static_file_path}' for template '{file_path}'")
+        return ''
 
-    return f'data-background-image="{ url_for("static", filename=background_path) }" data-background-opacity="0.6"'
+    return f'data-background-image="{ url_for("static", filename=background_path) }" data-background-opacity="0.6" data-background-color="#000000"'
